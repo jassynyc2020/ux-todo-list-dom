84b2fc83ea1f2ed5e41725e46e39cf6f891bdb9a
Hubot <nishihaider@yahoo.com>
2020-08-05 15:00:48 -0400

    done but not aligned next to box yet

--- src/index.js
+++ src/index.js
@@ -21,19 +21,20 @@ function renderTodoApp() {
   let app = document.querySelector("#app");
   let h1 = document.createElement("h1");
   let ul = document.createElement("ul");
+  ul.classList.add("todo-container");
+
   h1.innerText = `Todo List`;
   app.appendChild(h1);
   app.appendChild(ul);
 
   for (let i = 0; i < todos.length; i++) {
-    let li = `<li><input type="checkbox"><input></li>`;
-    ul.innerHTML += li;
-    document.getElementsByTagName(li).
-    let todosMarkup = `
-    <li class="list-item">
-      <p>${todos.description}</p>
-    </li>
+    let choresList = todos[i];
+    let choresListMarkup = `
+    <li class="list-item"><input type="checkbox">
+      <p>${choresList.description}</p>
+    </input></li>
     `;
+    ul.innerHTML += choresListMarkup;
   }
 }
 
